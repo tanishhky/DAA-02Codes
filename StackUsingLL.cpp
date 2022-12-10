a1d44811ed8f0c05f61da4f30cec200b8680d0a8
tanishhky <87553028+tanishhky@users.noreply.github.com>
2022-12-10 15:24:16 +0530

    Update StackUsingLL.cpp

--- StackUsingLL.cpp
+++ StackUsingLL.cpp
@@ -7,26 +7,16 @@ struct node{
     struct node* next;
 };
 
-struct node* enqueue(struct node* head,int d){
+struct node* push(struct node* head,int d){
     //initialize a newNode
     struct node* newNode=(struct node*)malloc(sizeof(struct node));
     newNode->data=d;
-    newNode->next=NULL;
-    
-    if(head==NULL){
-        return newNode;
-    }
-    else{
-        struct node* temp=head;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=newNode;
-    }
+    newNode->next=head;
+    head=newNode;
     return head;
 }
 
-struct node* dequeue(struct node* head){
+struct node* pop(struct node* head){
     head=head->next;
     return head;
 }
@@ -46,19 +36,10 @@ int main(){
     
     for(int i=0;i<n;i++){
         cin>>d;
-        head=insertEnd(head,d);
-        head=insertFront(head,d);
+        head=push(head,d);
     }
-    
     display(head);
-    head=insertRandom(head,6,3);
-    display(head);
-    head=deleteEnd(head);
-    display(head);
-    head=deleteFront(head);
-    display(head);
-    head=deleteRandom(head,2);
-    display(head);
+    head=pop(head)
     
     return 0;
 }
